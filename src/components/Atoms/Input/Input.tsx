052e74049c45c84312fa1f71b54b1fe2b873de56
Deborah Selvi D <deborah.selvi@zemosolabs.com>
2025-03-27 13:58:04 +0530

    Code optimization for enabling form submit button

--- src/components/Atoms/Input/Input.tsx
+++ src/components/Atoms/Input/Input.tsx
@@ -2,20 +2,38 @@ import { ChangeEventHandler } from "react";
 
 import "./Input.css";
 
-interface InputProps {
-  text: string;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
   value: string;
+  type?: string;
+  name?: string;
   handleInput: ChangeEventHandler<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({ text, value, handleInput }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  value,
+  type = "text",
+  name,
+  handleInput,
+  ...props
+}) => {
+  const inputName = name || label.toLowerCase().replace(/\s+/g, "");
   return (
-    <div id="input-wrapper">
+    <div className="input-wrapper">
       <div>
-        <label htmlFor={text}>{text}</label>
+        <label htmlFor={inputName}>{label}</label>
       </div>
       <div>
-        <input id={text} value={value} onChange={handleInput} />
+        <input
+          id={inputName}
+          name={inputName}
+          type={type}
+          value={value}
+          onChange={handleInput}
+          className="input"
+          {...props}
+        />
       </div>
     </div>
   );
